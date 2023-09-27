6f36045879efcbf24d2c28a71c0dc39611fc06c7
Veysel göç <95548660+Veysoo@users.noreply.github.com>
2023-09-27 21:53:42 +0300

    Summary

---
+++ mp3_downloader/lib/music_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MusicListPage extends StatelessWidget {
+  const MusicListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("müzik listesi buraya gelecek"),),
+    );
+  }
+}
